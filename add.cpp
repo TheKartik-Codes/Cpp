c53b27bab58f27a4aa47ba12d4a4623e7d795524
Kartik Kadam <itskartikkadam@gmail.com>
2025-04-08 12:19:38 +0530

    addition of two numbers

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    a = 5;
+    b = 10;
+
+    cout << "\nAddition of two numbers is : " << a + b << "\n";
+
+    return 0;
+}
