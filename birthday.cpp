0d6053df964a6dd9ee056d174f3631b1b384500e
Kartik Kadam <itskartikkadam@gmail.com>
2025-04-10 20:33:30 +0530

    Kartik's message

---
+++ birthday.cpp
@@ -0,0 +1,13 @@
+// A program to wish a happy birthday to Kartik
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\nHappy Birthday, Kartik !" << endl;
+    cout << "\nMay you accomplish all the goals you have set for yourself by the next birthday." << endl;
+
+    return 0;
+}
