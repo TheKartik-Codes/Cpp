602ce26871f09b6fa26a95ff2b77086707f87dfa
Kartik Kadam <itskartikkadam@gmail.com>
2025-04-11 21:28:33 +0530

    11 April 2025

---
+++ reminder.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\nYou should also consider the possibility that you may be wrong about your own interpretations." << endl;
+    cout << "\nYou must not let other people's opinions affect your own, and you should not consider their opinions while forming yours."<< endl;
+    
+    return 0;
+}
