888ae7b798a386581aaa5398cca85be997c6b76e
Kartik Kadam <itskartikkadam@gmail.com>
2025-04-22 20:42:45 +0530

    22 April 2025

---
+++ remider.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\n Hey there !";
+    cout << "\n Linux is very Cool Operating System.";
+
+    cout << "\n Terminal is a command line interface. It's like a whatsapp chat interface where you write text messages and get replies.";
+    cout << "\n Shell is a program that takes commands from the keyboard and gives them to the operating system to perform. It's like a translator between you and the computer. It's like a person who takes your messages and traslates them into a language the computer understands.";
+    cout << "\n Bash is a type of shell. It's like a specific translator that speaks a certain language. There are many types of shells, just like there are many types of translators.";
+
+    return 0;
+}
